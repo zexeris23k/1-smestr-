ba71a169fc2271a2452292d01add74f249f95234
zexeris23k <zexerlol70@gmail.com>
2024-11-15 12:59:28 +0300

    Add files via upload

---
+++ разтворов pz3/Program1/Program1/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+class HelloUser
+{
+    static void Main()
+    {
+        String name;
+        WriteLine("Введите Ваше имя:");
+        name = ReadLine();
+        WriteLine("Приветсвую Вас, " + name + "!");
+        WriteLine("/infl завершения сеанса нажмите ENTER.");
+        ReadLine();
+    }
+}
---
+++ разтворов pz3/Program2/Program2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            System.Console.WriteLine("Введите Ваше имя:");
+            name = System.Console.ReadLine();
+            System.Console.WriteLine("Приветствую Вас," + name + "!");
+        }
+    }
+}
